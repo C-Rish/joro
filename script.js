f46526d3e049a174e5029aab1eea36804e88b2a0
Shirish Khadgi <shirishkhadgi23@gmail.com>
2021-06-29 16:06:55 +0545

    sliders added

--- script.js
+++ script.js
@@ -1,4 +1,24 @@
 $('.canada').click(function(){
   $(this).toggleClass('actives');
-  // $('.seller-canada').css("display","none");
+  if($('.canada').hasClass('actives')){
+    $('.usa').removeClass('actives');
+    $('.sellers-usa').css("display","none");
+    $('.sellers-canada').css("display","block");
+  }
 });
+
+$('.usa').click(function(){
+  $(this).toggleClass('actives');
+  if($('.usa').hasClass('actives')){
+    $('.canada').removeClass('actives');
+    $('.sellers-canada').css("display","none");
+    $('.sellers-usa').css("display","block");
+  }
+});
+
+$(document).ready(function(){
+  $('.slider').bxSlider();
+})
+
+
+
